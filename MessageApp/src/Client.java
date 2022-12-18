769cc0d64635636c3fd7ce39ba19429f601fbe2c
PTLyon <pedro_rafael_santos@hotmail.com>
2022-12-18 22:58:51 +0000

    Update Client.java

--- MessageApp/src/Client.java
+++ MessageApp/src/Client.java
@@ -51,10 +51,91 @@ public class Client extends JFrame {
         }
     }
 
+    /**
+     *
+     * @throws IOException
+     */
     private void connectToServer() throws IOException {
         showMessage("\n Attempting connection...");
         connection = new Socket(InetAddress.getByName(serverIP), 6789);
-        showMessage("Connected to:" + connection.getInetAddress().getHostName())
+        showMessage("Connected to:" + connection.getInetAddress().getHostName());
     }
 
+    /**
+     * Sets up I/O streams
+     * @throws IOException
+     */
+    private void setupStreams () throws IOException {
+        output =  new ObjectOutputStream(connection.getOutputStream());
+        output.flush();
+        input = new ObjectInputStream(connection.getInputStream());
+        showMessage("\n Streams are now connected, go!");
+    }
+
+    /**
+     * While chatting
+     */
+    private void whileChatting() throws IOException{
+        ableToType(true);
+        do{
+            try{
+                message = (String) input.readObject();
+                showMessage("\n " + message);
+            }catch(ClassNotFoundException classNotFoundException){
+                showMessage("\n I don't know that object type");
+            }
+        }while(!message.equals("SERVER - END"));
+    }
+
+    //close streams and sockets
+    private void closeCrap(){
+        showMessage("\n Closing crap down...");
+        ableToType(false);
+        try{
+            output.close();
+            input.close();
+            connection.close();
+        }catch(IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
+
+
+    /**
+     * send messages to the server
+     * @param message
+     */
+    private void sendMessage(String message) {
+        try{
+            output.writeObject("CLIENT - " + message);
+            output.flush();
+            showMessage("\nCLIENT - " + message);
+        }catch(IOException ioException){
+            chatWindow.append("\n something messed up sending message");
+        }
+    }
+
+    //change or update chat window
+    private void showMessage(final String m){
+        SwingUtilities.invokeLater(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        chatWindow.append(m);
+                    }
+                }
+        );
+    }
+    //gives user permission to type
+    private void ableToType(final boolean tof){
+        SwingUtilities.invokeLater(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        userText.setEditable(tof);
+                    }
+                }
+        );
+
+    }
 }
